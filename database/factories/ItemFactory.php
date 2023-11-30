c2defbd2ae5629e75d6696b1a57bdb37e7b9b80b
jonathan <jonathanodongtho02@gmail.com>
2023-11-30 13:49:04 +0300

    Add files via upload

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Item;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Item>
+ */
+class ItemFactory extends Factory
+{
+    protected $model = Item::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'item_name' => fake()->name(),
+            'category_id' => rand(1, 4),
+        ];
+    }
+}
